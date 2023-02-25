72a031c855b5cd86e00aea6587597fee8203e89d
yang <2655164055@qq.com>
2023-02-25 15:29:09 +0800

    third commit

--- git-test/src/main/java/com/itheima/git.java
+++ git-test/src/main/java/com/itheima/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("1.本地库文件的创建、添加、提交");
         System.out.println("2.本地库文件的创建、添加、提交");
+        System.out.println("3.本地库文件的创建、添加、提交");
     }
 }
