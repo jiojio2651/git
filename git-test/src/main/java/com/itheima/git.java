ccaec0054f7b192b26911a0d4e532b0cc1268876
yang <2655164055@qq.com>
2023-02-25 15:45:19 +0800

    forth commit

--- git-test/src/main/java/com/itheima/git.java
+++ git-test/src/main/java/com/itheima/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("1.本地库文件的创建、添加、提交");
         System.out.println("2.本地库文件的创建、添加、提交");
         System.out.println("3.本地库文件的创建、添加、提交");
+        System.out.println("4.本地库文件的创建、添加、提交");
     }
 }
