a7edb4c2acf5b7883cb70221827a09a4b9b15e38
yang <2655164055@qq.com>
2023-02-25 15:28:15 +0800

    second commit

--- git-test/src/main/java/com/itheima/git.java
+++ git-test/src/main/java/com/itheima/git.java
@@ -2,6 +2,7 @@ package com.itheima;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("本地库文件的创建、添加、提交");
+        System.out.println("1.本地库文件的创建、添加、提交");
+        System.out.println("2.本地库文件的创建、添加、提交");
     }
 }
